b1b24ab5630c38195c9ce855e3ec775df2036499
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2025-12-19 11:42:45 +0000

    Added playGame function
    added the playGame function and refactored to
    delare the play round and increment scores into
    this function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
+
 
 
 function getComputerChoice() {
@@ -18,7 +17,7 @@ function getHumanChoice() {
     let humanChoice = prompt("Enter your choice");
     return humanChoice.toLowerCase()
 }
-
+/*
 function playRound(humanChoice, computerChoice) {
     let winner;
     let outcomeMessage;
@@ -54,7 +53,69 @@ function playRound(humanChoice, computerChoice) {
     }
 
     return outcomeMessage
+}*/
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    // declare playRound
+    function playRound(humanChoice, computerChoice) {
+        let winner;
+
+        // scenarios where the human wins
+        if (humanChoice == "rock" && computerChoice == "scissors") {
+            winner = "human"
+        } else if (humanChoice == "paper" && computerChoice == "rock") {
+            winner = "human"
+        } else if (humanChoice == "scissors" && computerChoice == "paper") {
+            winner = "human"
+        } 
+        // scenarios where computer wins
+        else if (humanChoice == "rock" && computerChoice == "paper") {
+            winner = "computer"
+        } else if (humanChoice == "paper" && computerChoice == "scissors"){
+            winner = "computer"
+        } else if (humanChoice == "scissors" && computerChoice == "rock") {
+            winner = "computer"
+        }
+        // draw scenario
+        else {
+            winner = "draw"
+        }
+        return winner
+    }
+    function updateScore(winner, humanChoice, computerChoice) {
+        let outcomeMessage;
+        if (winner == "human") {
+            ++humanScore
+            outcomeMessage = "You Win! " + humanChoice + " beats " + computerChoice
+        } else if (winner == "computer") {
+            ++computerScore
+            outcomeMessage = "You lose! " + humanChoice + " loses to " + computerChoice
+        } else {
+            outcomeMessage = "Draw! "
+        }
+        return outcomeMessage
+    }
+    // keep track of score
+    // repeat 5 times, and declare winner at the end
+    for (let i = 0; i < 5; i++) {
+        let masterHumanChoice = getHumanChoice();
+        let masterComputerChoice = getComputerChoice();
+        let roundOutcome = playRound(masterHumanChoice, masterComputerChoice);
+
+        console.log(updateScore(roundOutcome, masterHumanChoice, masterComputerChoice) + "(score: " + humanScore + " : " + computerScore + ")")
+        if (i == 4) {
+            let finalOutcome
+            if(humanScore > computerScore) {
+                finalOutcome = "You win!"
+            } else if (computerScore > humanScore) {
+                finalOutcome = "You lose!"
+            } else {
+                finalOutcome = "It's a draw!"
+            }
+            console.log("Game over! " + finalOutcome + "(Final score: " + humanScore + " : " + computerScore + ")")
+        }
+    }
 }
-
-
-console.log(playRound(getHumanChoice(),getComputerChoice()))
+playGame()
