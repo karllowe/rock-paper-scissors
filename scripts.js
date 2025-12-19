78b38fb7c48195f2e39d2dd2d363b322dca05744
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2025-12-19 10:41:34 +0000

    added winning function
    added function to decide who wins

--- scripts.js
+++ scripts.js
@@ -6,18 +6,57 @@ function getComputerChoice() {
     let randomNumber = Math.random()
 
     if (randomNumber <= 0.33) {
-        return "Rock"
+        return "rock"
     } else if (randomNumber <=0.66) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Enter your choice").toLowerCase();
-    return humanChoice
+    let humanChoice = prompt("Enter your choice");
+    return humanChoice.toLowerCase()
+}
+
+function convertInputToNumber(inputWord) {
+    switch (inputWord) {
+        case "rock":
+            return 1;
+            break;
+        case "paper":
+            return 2;
+            break;
+        case "scissors":
+            return 3;
+            break;
+    }
 }
 
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    let winner;
+    // scenarios where the human wins
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        winner = "human"
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        winner = "human"
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        winner = "human"
+    } 
+    // scenarios where computer wins
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        winner = "computer"
+    } else if (humanChoice == "paper" && computerChoice == "scissors"){
+        winner = "computer"
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        winner = "computer"
+    }
+    // draw scenario
+    else {
+        winner = "draw"
+    }
+    return winner + "(user: " + humanChoice + " computer: " + computerChoice + ")"
+}
+
+console.log(playRound(getHumanChoice(),getComputerChoice()))
