ff56111434920cee81b88bf4e2c32b9ae408b74d
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-01-06 14:00:31 +0000

    Added event listeners for the buttons
    Added event listeners for the player selection buttons and added result
    div

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-
-
-
 function getComputerChoice() {
     let randomNumber = Math.random()
 
@@ -17,43 +14,6 @@ function getHumanChoice() {
     let humanChoice = prompt("Enter your choice");
     return humanChoice.toLowerCase()
 }
-/*
-function playRound(humanChoice, computerChoice) {
-    let winner;
-    let outcomeMessage;
-
-    // scenarios where the human wins
-    if (humanChoice == "rock" && computerChoice == "scissors") {
-        winner = "human"
-    } else if (humanChoice == "paper" && computerChoice == "rock") {
-        winner = "human"
-    } else if (humanChoice == "scissors" && computerChoice == "paper") {
-        winner = "human"
-    } 
-    // scenarios where computer wins
-    else if (humanChoice == "rock" && computerChoice == "paper") {
-        winner = "computer"
-    } else if (humanChoice == "paper" && computerChoice == "scissors"){
-        winner = "computer"
-    } else if (humanChoice == "scissors" && computerChoice == "rock") {
-        winner = "computer"
-    }
-    // draw scenario
-    else {
-        winner = "draw"
-    }
-
-    // increment scores and set the winning message
-    if (winner == "human") {
-        ++humanScore
-        outcomeMessage = "You Win! " + humanChoice + " beats " + computerChoice + "."
-    } else if (winner == "computer") {
-        ++computerScore
-        outcomeMessage = "You lose! " + humanChoice + " loses to " + computerChoice + "."
-    }
-
-    return outcomeMessage
-}*/
 
 function playGame() {
     let humanScore = 0;
@@ -99,23 +59,35 @@ function playGame() {
     }
     // keep track of score
     // repeat 5 times, and declare winner at the end
-    for (let i = 0; i < 5; i++) {
-        let masterHumanChoice = getHumanChoice();
-        let masterComputerChoice = getComputerChoice();
-        let roundOutcome = playRound(masterHumanChoice, masterComputerChoice);
+    // for (let i = 0; i < 5; i++) {
+    //     let masterHumanChoice = getHumanChoice();
+    //     let masterComputerChoice = getComputerChoice();
+    //     let roundOutcome = playRound(masterHumanChoice, masterComputerChoice);
 
-        console.log(updateScore(roundOutcome, masterHumanChoice, masterComputerChoice) + "(score: " + humanScore + " : " + computerScore + ")")
-        if (i == 4) {
-            let finalOutcome
-            if(humanScore > computerScore) {
-                finalOutcome = "You win!"
-            } else if (computerScore > humanScore) {
-                finalOutcome = "You lose!"
-            } else {
-                finalOutcome = "It's a draw!"
-            }
-            console.log("Game over! " + finalOutcome + "(Final score: " + humanScore + " : " + computerScore + ")")
-        }
-    }
+    //     console.log(updateScore(roundOutcome, masterHumanChoice, masterComputerChoice) + "(score: " + humanScore + " : " + computerScore + ")")
+    //     if (i == 4) {
+    //         let finalOutcome
+    //         if(humanScore > computerScore) {
+    //             finalOutcome = "You win!"
+    //         } else if (computerScore > humanScore) {
+    //             finalOutcome = "You lose!"
+    //         } else {
+    //             finalOutcome = "It's a draw!"
+    //         }
+    //         console.log("Game over! " + finalOutcome + "(Final score: " + humanScore + " : " + computerScore + ")")
+    //     }
+    // }
 }
-playGame()
+playGame();
+
+// button listeners
+const btns = document.querySelectorAll("button");
+
+function btnClick(e) {
+    const playerSelection = e.target.id;
+    console.log(playerSelection);
+};
+
+btns.forEach((button) => {
+    button.addEventListener("click", btnClick)
+});
